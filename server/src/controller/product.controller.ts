734f25fbb4bd5c83aa12d154e155afb38b4ca014
rnimisha <nimisaraut@gmail.com>
2023-06-03 01:07:02 +0545

    database connection

--- server/src/controller/product.controller.ts
+++ server/src/controller/product.controller.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express"
+import pool from '../db'
 
-export const getAllProducts = (req: Request, res: Response): void=>{
-    res.json({
-        data : ['milk']
-    })
+export const getAllProducts = async (req: Request, res: Response): Promise<void>=>{
+    try{
+        const data = await pool.query('Select * from product')
+
+        res.json({
+            success: true,
+            data: data.rows
+        })
+    }catch(err)
+    {
+        res.status(500).json({
+            success: false,
+            msg: (err as Error).message
+        })
+    }
+    
 }
---
+++ server/src/db/index.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg"
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool: Pool = new Pool({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    port: 5432,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+export default pool
